945cc1f005cbd89a96b765f27ea9e34a5a0b5295
uodev <uygaroztcyln@gmail.com>
2023-03-29 02:28:23 +0300

    Add project files.

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T> where T: class
+    {
+        //CRUD
+        //type name();
+        List<T> List();
+        void Insert(T t);
+        void Update(T t);
+        void Delete(T t);
+
+        List<T> List(Expression<Func<T, bool>> filter);
+    }
+}
